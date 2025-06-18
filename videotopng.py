998b9997a0fec99244acacd3cfe9d6601fd20134
unknown <jhamsidrbz@gmail.com>
2025-06-18 17:11:01 -0500

    png to webp

--- videotopng.py
+++ videotopng.py
@@ -7,7 +7,7 @@ while video.isOpened():
     ret, frame = video.read()
     if ret:
         cv2.imshow('Frame', frame)
-        cv2.imwrite(f"img/{x}.png",frame)
+        cv2.imwrite(f"img/{x}.webp",frame)
         x+= 1
         if cv2.waitKey(25) & 0xFF == ord('q'):
             break
